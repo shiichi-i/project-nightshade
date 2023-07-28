31f855e04c90d7d4f8bfbcabd6edc2a89b51c1ad
Ayronman3 <sj.guevarra00275@student.tsu.edu.ph>
2023-07-29 01:25:51 +0800

    Fixing the responsiveness of the divs

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -102,58 +102,59 @@ const Home = () => {
 
             <Image src={Purple} w="full" mt={{base: '-25px', md: '-170px'}} />
             <Box w="full" h="500px" bg="midnight" display="flex" alignItems='center' justifyContent='center'>
-                {/* <Text>Juan Dela Cruz</Text> */}
+                {/* MEMBER OF THE MONTH IMAGE PLACEHOLDER*/}
                 <Box w="90vw" h="500px" display="flex" flexDir={{base: 'column', md: 'row'}} bg='midnight'>
+                    {/*PARENT BOX OF THE SHAPES DIV*/}
                     <Box ml={{md:'25px'}} w={{base: '100%',md:'50%'}} h='full'
                     display="flex" alignItems="center" justifyContent="center"  position="relative" minHeight='250px'>
-                        {/** STAR*/}
+                        {/*SHAPES FORMATTING AND POSITIONING*/}
+                        
                         <Box position="absolute" h="full" display="flex" alignItems="center" justifyContent="center" mr={0}>
                             <Box position="absolute" top={{base:'-5px',sm: '-30px', md: '60px',lg:'-20px'}} left={{base:'-50px', md: '-50px',lg:'-60px'}}  zIndex={200}>
                                 <Image src={Star2} w={{base: '80px',sm:'90px', lg: '120px'}} />
                             </Box>
                             <Box mr='0' position="absolute" bottom={{base:'-5px',sm:'-30px', md: '70px',lg:'-15px'}} right={{base: '-50px', md: '-50px',lg:"-60px"}} zIndex={200}>
-                            <Image src={Star} w={{base: '80px',sm:'90px', lg: '120px'}} />
+                                <Image src={Star} w={{base: '80px',sm:'90px', lg: '120px'}} />
                             </Box>
                             <Box  zIndex={201}>
-                            <Image src={Rectangle} w={{base:'200px',sm:'250px', md:"350px",lg:"420px"}}/>
+                                <Image src={Rectangle} w={{base:'200px',sm:'250px', md:"350px",lg:"420px"}}/>
                                 </Box>      
                         </Box>
                     </Box>
-                   
+                {/*PARENT BOX OF THE DETAILS DIV*/}
                 <Box w={{base: '100%',md:'50%'}} h="full"
                 display="flex" alignItems="flex-start" justifyContent="center"  position="relative">
-                    {/** text */}
+                    {/*DETAILS OF THE MEMBER */}
                     <Box ml={{md:'50px'}} color="black"  display={{base: 'flex'}} flexDirection={{base:'column',}} alignItems={{base:'center',md:'flex-start'}} mt={{base:'15px',sm:'30px',md:'100px'}}>
                         <Heading fontSize={{base:'25px',md:'30px', lg:'35px'}} color="rgb(18,198,176)"pb="15px" whiteSpace='nowrap'>MEMBER OF THE MONTH</Heading>
                         <Heading fontSize={{base:"25px",md:'30px', lg:'35px'}}pb={{base:'10px',md:"10px"}} color="rgb(22,20,43)">Juan Dela Cruz</Heading>
-                        <Box w={{base:"95%",md:"95%"}}>
-                        <Text fontSize={{base:"12px",md:"14px"}} color="rgb(22,20,43)" mb='20px'>
-                             Hi I’m Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </Text>
+                            <Box w={{base:"95%",md:"95%"}}>
+                                <Text fontSize={{base:"12px",md:"14px"}} color="rgb(22,20,43)" mb='20px'>
+                                    Hi I’m Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </Text>
+                            </Box>
                         </Box>
-                        
-                        
                     </Box>
                 </Box>
-                
-                </Box>
             </Box>
+            {/*PUTTING THE CURVED DESIGN IN THE PAGE*/}
             <Image src={HeadPurple} w="full" mt={{base: '-25px', md: '-10px'}} />
             <Box w="90vw" h="500px" display="flex" flexDir={{base: 'column', md: 'row-reverse'}} >
+                {/*PLACEHOLDER FOR THE PROJECT SHOWCASE*/}
                 <Box w={{base: '100%',md:'50%'}} h="full"
                     display="flex" alignItems="center" justifyContent="center"  position="relative" >
                         <Box position="absolute" h="full" display="flex" alignItems="baseline" justifyContent="center" mt={{base:'0px', md:'-140px',lg:'-200px'}}>
                             <Box>
-                            <Image src={Rectangle} w={{base:'200px',sm:'300px', md:"360px",lg:"400px"}}/>
-                                </Box>      
+                                <Image src={Rectangle} w={{base:'200px',sm:'300px', md:"360px",lg:"400px"}}/>
+                            </Box>      
                         </Box>
                     </Box>
-                    
+                    {/*PARENT BOX OF THE PROJECT DETAILS DIV*/}
                     <Box w={{base: '100%',md:'50%'}} h="full"
                     display="flex" alignItems="baseline" justifyContent="center"  position="relative" >
-                        {/** text */}
+                        {/*PROJECT DETAILs*/}
                         <Box ml="3vw" color="black"  display={{base: 'flex'}} flexDirection={{base:'column',}} alignItems={{base:'center',md:'flex-start'}} mt={{base:'-50px',sm:'50px'}}>
                             <Heading fontSize={{base:'25px',md:'35px',lg:'45px'}} color="#FFFFFF"pb="20px" whiteSpace={{base:'nowrap'}}>FEATURED PROJECT</Heading>
                             <Box w={{base:"95%",md:"95%"}}>
@@ -163,6 +164,7 @@ const Home = () => {
                                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             </Text>
                             </Box>
+                            {/*BUTTON FOR VIEW THE PROJECT*/}
                             <Box layerStyle="bubblegum" w="200px" h="48px" borderRadius="40px">
                                 <motion.div
                                 whileHover={{ scale: 1.2 }}
