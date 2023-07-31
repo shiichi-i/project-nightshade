2f3a4b1218e30273dcc271f331fcc4520edc4bc6
shiichi-i <88258251+shiichi-i@users.noreply.github.com>
2023-07-31 18:36:37 +0800

    init

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,9 +1,15 @@
 import { Box, Image, Button } from "@chakra-ui/react";
 
+import Banner from "../../assets/pd_banner-blk.png";
+
 const Footer = () => {
     return(
-        <Box bg="#ba55e3" w="full" h="30px" borderTopRadius="40"
-        position="absolute" bottom="0"/>
+        <Box mt="-35px">
+            <Box bg="#ba55e3" w="full" h="35px" borderTopRadius="40" bottom="0"
+            display="flex" alignItems="center">
+                <Image src={Banner} h="25px" ml="16px" />
+            </Box>
+        </Box>
     );
 }
 export default Footer;
